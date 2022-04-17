515959a7939b6db6ec6c97efc232e1a3b7983254
Becca615 <damilolarebecca8@gmail.com>
2022-04-17 14:26:41 +0000

    Done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,27 +80,16 @@ class DBStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
-                if cls not in classes.values():
-            return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
-
+        obj_dict = models.storage.all(cls)
+        for k, v in obj_dict.items():
+            matchstring = cls + '.' + id
+            if k == matchstring:
+                return v
         return None
-
-    def count(self, cls=None):
+        
+def count(self, cls=None):
         """
         count the number of objects in storage
         """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
-
-        return count
+        obj_dict = models.storage.all(cls)
+        return len(obj_dict)
